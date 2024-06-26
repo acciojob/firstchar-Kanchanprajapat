6fae6e7ca612002e099fd023433ae51e21e842e5
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-06-26 12:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstChar(text) {
-  // your code here
+  // your code her
+	if(text === ''){
+		return '';
+	}
+	 for (let i = 0; i < text.length; i++) {
+    // Check if the character is not a space
+    if (text[i] !== ' ') {
+      // Return the first non-space character
+      return text[i];
+    }
+  }
+
+  // If no non-space character is found, return an emptystring
+  return '';
 }
 
 // Do not change the code below
